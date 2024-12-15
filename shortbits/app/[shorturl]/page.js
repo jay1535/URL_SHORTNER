10b4ed94aa7be32988ee5241b4de71f5807a4e22
Jayant R Habbu <habbujayanth@gmail.com>
2024-12-15 18:04:57 +0530

    Update page.js

--- shortbits/app/[shorturl]/page.js
+++ shortbits/app/[shorturl]/page.js
@@ -1,19 +1,20 @@
-import { redirect } from "next/navigation"
-import clientPromise from "@/lib/mongodb"
+import { redirect } from "next/navigation";
+import clientPromise from "@/lib/mongodb";
 
 export default async function Page({ params }) {
-    const shorturl = (await params).shorturl
-    const client = await clientPromise
-    const db = client.db("shortbits")
-    const collection = db.collection("url")
-    
-    const doc = collection.findOne({shorturl: shorturl})
-    if(doc){
-       redirect(doc.url)
-      }
-    else{
-        redirect(`${NEXT_PUBLIC_HOST}`)
-    }
+  const { shorturl } = params; 
 
-    return <div>My Post: {url}</div>
+  const client = await clientPromise;
+  const db = client.db("shortbits");
+  const collection = db.collection("url");
+
+  const doc = await collection.findOne({ shorturl: shorturl });
+  if (doc) {
+    redirect(doc.url); 
+    return; 
   }
+
+  const host = process.env.NEXT_PUBLIC_HOST || "http://localhost:3000";
+  redirect(host);
+  return; 
+}
