fbd49afaacefa459aa9002f73e73da5be51d327c
Jayant R Habbu <habbujayanth@gmail.com>
2024-12-15 18:03:10 +0530

    Update route.js

--- shortbits/app/api/generate/route.js
+++ shortbits/app/api/generate/route.js
@@ -1,21 +1,28 @@
-import clientPromise from "@/lib/mongodb"
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
+  try {
+    const body = await request.json();
+    const client = await clientPromise;
+    const db = client.db("shortbits");
+    const collection = db.collection("url");
 
-    const body= await request.json();
-    const client = await clientPromise
-    const db = client.db("shortbits")
-    const collection = db.collection("url")
+    const doc = await collection.findOne({ shorturl: body.shorturl });
+    if (doc) {
+      return NextResponse.json({ success: false, error: true, message: "URL already exists" });
+    }
 
-    const doc = collection.findOne({shorturl: body.shorturl})
-    if(doc){
-        return Response.json({ sucess:false, error:true, message: 'URL already exists' })  
-      }
+ 
+    const result = await collection.insertOne({
+      url: body.url,
+      shorturl: body.shorturl,
+    });
 
- const result= await  collection.insertOne({
-    url:body.url,
-    shorturl : body.shorturl
-})
+    return NextResponse.json({ success: true, error: false, message: "Finished", data: result });
+  } catch (error) {
 
-    return Response.json({ sucess:true, error:false, message: 'Finished' })
+    console.error("Error in POST handler:", error);
+    return NextResponse.json({ success: false, error: true, message: "Internal server error" }, { status: 500 });
   }
+}
